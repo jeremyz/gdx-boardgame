9e6b127d1aad4dc9890b525b852ece2a3f9a2038
Jérémy Zurcher <jeremy@asynk.ch>
2018-10-02 09:59:03 +0200

    test : fix path to respect package name

--- test/src/ch/asynk/gdx/tabletop/test/UiScreen.java
+++ test/src/ch/asynk/gdx/tabletop/test/UiScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.OrthographicCamera;
