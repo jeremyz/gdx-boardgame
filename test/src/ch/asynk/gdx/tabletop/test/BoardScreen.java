d292f18161846fc14342063586c361f4953428ad
Jérémy Zurcher <jeremy@asynk.ch>
2018-10-03 16:08:36 +0200

    BoardScreen : be sure to reset glViewport when exiting

--- test/src/ch/asynk/gdx/tabletop/test/BoardScreen.java
+++ test/src/ch/asynk/gdx/tabletop/test/BoardScreen.java
@@ -228,6 +228,7 @@ public class BoardScreen extends AbstractScreen
             case TRI_H: board.setTRI_H(); break;
             case TRI_V: board.setTRI_V(); break;
             case DONE:
+                cam.applyScreenViewport();
                 this.app.switchToMenu();
                 return;
         }
