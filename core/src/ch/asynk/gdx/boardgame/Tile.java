3b34d467feca5b255f598f77865208aeecdbb4c6
Jérémy Zurcher <jeremy@asynk.ch>
2018-10-18 16:51:35 +0200

    Tile : add overlays, implement Drawable

--- core/src/ch/asynk/gdx/boardgame/Tile.java
+++ core/src/ch/asynk/gdx/boardgame/Tile.java
@@ -1,14 +1,40 @@
 package ch.asynk.gdx.boardgame;
 
-public class Tile
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import ch.asynk.gdx.boardgame.Overlays;
+
+public class Tile implements Drawable
 {
+    public static TextureAtlas defaultOverlay = null;
+
     public float x;
     public float y;
+    private Overlays overlays;
 
     public Tile(float x, float y)
     {
         this.x = x;
         this.y = y;
+        if (defaultOverlay != null) {
+            this.overlays = new Overlays(defaultOverlay);
+            this.overlays.centerOn(x, y);
+        }
+    }
+
+    @Override public void draw(Batch batch)
+    {
+        if (overlays != null) {
+            overlays.draw(batch);
+        }
+    }
+
+    public void enableOverlay(int i, boolean enable)
+    {
+        if (overlays != null) {
+            overlays.enable(i, enable);
+        }
     }
 
     @Override public String toString()
