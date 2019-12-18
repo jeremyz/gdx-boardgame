6b46e61ecefb120b186247ac01c6e2afac6337bb
Jérémy Zurcher <jeremy@asynk.ch>
2019-12-18 09:56:48 +0100

    UI : add Bg (TextureRegion)

---
+++ core/src/ch/asynk/gdx/boardgame/ui/Bg.java
@@ -0,0 +1,22 @@
+package ch.asynk.gdx.boardgame.ui;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Bg extends Element
+{
+    private TextureRegion region;
+
+    public Bg(TextureRegion region)
+    {
+        this.region = region;
+        rect.set(0, 0, region.getRegionWidth(), region.getRegionHeight());
+    }
+
+    @Override public void draw(Batch batch)
+    {
+        if (!visible) return;
+        if (tainted) computeGeometry();
+        batch.draw(region, rect.x, rect.y, rect.width, rect.height);
+    }
+}
